12fed2e72771cc7bdef50f6c4a4199f46052553e
minimilien <47968684+minimilien@users.noreply.github.com>
2020-11-18 10:37:02 +0100

    "update"

--- deploy.go
+++ deploy.go
@@ -7,8 +7,10 @@ import (
 	"strings"
 )
 
-func CMD_call(commande []string, debug bool) (string, error) {
-	cmd := exec.Command(commande[0], commande[1:]...)
+//appel console
+func CMD_call(commande string, debug bool) (string, error) {
+	commande_split := strings.Split(commande, " ")
+	cmd := exec.Command(commande_split[0], commande_split[1:]...)
 	out, err := cmd.CombinedOutput()
 	fmt.Printf("combined out:\n%s\n", string(out))
 	if (err != nil) && (debug == true) {
@@ -18,18 +20,18 @@ func CMD_call(commande []string, debug bool) (string, error) {
 }
 
 func main() {
-	commandes1 := []string{"docker", "build", "-t", "data-eng:latest", "."}
-	commandes11 := strings.Split("tree", " ")
-	commandes2 := []string{"docker", "run", "--name", "PROJET", "-d", "-p", "5000:5000", "data-eng"}
-	tests := []string{"conda", "run", "-n", "python37", "python", "tests.py"}
-	commandes3 := []string{"docker", "pause", "PROJET"}
-	commandes4 := []string{"docker", "container", "rm", "--force", "PROJET"}
-	commandes5 := []string{"docker", "image", "rm", "--force", "data-eng"}
-	commandes55 := []string{"heroku", "login"}
-	commandes6 := []string{"git", "add", "."}
-	commandes7 := []string{"git", "commit", "-m", "\"update\""}
-	commandes8 := []string{"git", "push"}
-	commandes9 := []string{"git", "push", "heroku", "HEAD:master"}
+	commandes1 := "docker build -t data-eng:latest ."
+	commandes11 := "tree"
+	commandes2 := "docker run --name PROJET -d -p 5000:5000 data-eng"
+	tests := "conda run -n python37 python tests.py"
+	commandes3 := "docker pause PROJET"
+	commandes4 := "docker container rm --force PROJET"
+	commandes5 := "docker image rm --force data-eng"
+	commandes55 := "heroku login"
+	commandes6 := "git add ."
+	commandes7 := "git commit -m \"update\""
+	commandes8 := "git push"
+	commandes9 := "git push heroku  HEAD:master"
 	CMD_call(commandes11, true)
 	CMD_call(commandes1, true)
 	CMD_call(commandes2, true)
